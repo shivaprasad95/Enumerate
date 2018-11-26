281eff41ada0ebe6a65082a11ce3a4e4d59a0bfb
Sudeep Maity <sudeep.nitg@gmail.com>
2018-11-25 22:12:13 -0600

    Code for PERT and EnumerateTopological

--- src/sxb180066/EnumerateTopological.java
+++ src/sxb180066/EnumerateTopological.java
@@ -10,76 +10,123 @@ import rbk.Graph.Vertex;
 import rbk.Graph.Edge;
 import rbk.Graph.Factory;
 
+import rbk.Graph;
+import sxb180066.Enumerate;
+import rbk.Graph.GraphAlgorithm;
+import rbk.Graph.Timer;
+import rbk.Graph.Vertex;
+import rbk.Graph.Edge;
+import rbk.Graph.Factory;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class EnumerateTopological extends GraphAlgorithm<EnumerateTopological.EnumVertex> {
-    boolean print;  // Set to true to print array in visit
-    long count;      // Number of permutations or combinations visited
-    Selector sel;
-    public EnumerateTopological(Graph g) {
-	super(g, new EnumVertex());
-	print = false;
-	count = 0;
-	sel = new Selector();
-    }
+	boolean print; // Set to true to print array in visit
+	long count; // Number of permutations or combinations visited
+	Selector sel;
+	private static List<Graph.Vertex> finishList;
 
-    static class EnumVertex implements Factory {
-	EnumVertex() { }
-	public EnumVertex make(Vertex u) { return new EnumVertex();	}
-    }
-
-    class Selector extends Enumerate.Approver<Vertex> {
-	@Override
-	public boolean select(Vertex u) {
-	    return true;
+	public EnumerateTopological(Graph g) {
+		super(g, new EnumVertex());
+		print = false;
+		count = 0;
+		sel = new Selector();
 	}
 
-	@Override
-	public void unselect(Vertex u) {
-	}
+	static class EnumVertex implements Factory {
+		int indegree;
 
-	@Override
-	public void visit(Vertex[] arr, int k) {
-	    count++;
-	    if(print) {
-		for(Vertex u: arr) {
-		    System.out.print(u + " ");
+		EnumVertex() {	}
+
+		public EnumVertex make(Vertex u) {
+			return new EnumVertex();
 		}
-		System.out.println();
-	    }
 	}
-    }
-    
-    
-    // To do: LP4; return the number of topological orders of g
-    public long enumerateTopological(boolean flag) {
-	print = flag;
-	return count;
-    }
 
-    //-------------------static methods----------------------
+	/**
+	 * Approver class for enumerate topological
+	 */
+	class Selector extends Enumerate.Approver<Vertex> {
 
-    public static long countTopologicalOrders(Graph g) {
-        EnumerateTopological et = new EnumerateTopological(g);
-	return et.enumerateTopological(false);
-    }
+		@Override
+		public boolean select(Vertex u) {
+			if (get(u).indegree == 0) {
+				for (Edge e : g.incident(u)) {
+					Vertex v = e.otherEnd(u);
+					get(v).indegree--;
+				}
+				return true;
+			}
+			return false;
+		}
 
-    public static long enumerateTopologicalOrders(Graph g) {
-        EnumerateTopological et = new EnumerateTopological(g);
-	return et.enumerateTopological(true);
-    }
+		@Override
+		public void unselect(Vertex u) {
+			for (Edge e : g.incident(u)) {
+				Vertex v = e.otherEnd(u);
+				get(v).indegree++;
+			}
+		}
 
-    public static void main(String[] args) {
-	int VERBOSE = 0;
-        if(args.length > 0) { VERBOSE = Integer.parseInt(args[0]); }
-        Graph g = Graph.readDirectedGraph(new java.util.Scanner(System.in));
-        Graph.Timer t = new Graph.Timer();
-        long result;
-	if(VERBOSE > 0) {
-	    result = enumerateTopologicalOrders(g);
-	} else {
-	    result = countTopologicalOrders(g);
+		@Override
+		public void visit(Vertex[] arr, int k) {
+			count++;
+			if (print) {
+				for (Vertex u : arr) {
+					System.out.print(u + " ");
+				}
+				System.out.println();
+			}
+		}
 	}
-        System.out.println("\n" + result + "\n" + t.end());
-    }
+
+
+	/**
+	 * Enumerate topological method which returns the count of enumeration
+	 * @param flag
+	 * @return
+	 */
+	public long enumerateTopological(boolean flag) {
+		//EnumerateTopological
+		print = flag;
+		Vertex[] arr = g.getVertexArray();
+		Enumerate e = new Enumerate (arr, sel);
+		//initilization
+		for (Vertex u : g ) {
+			get(u).indegree = u.inDegree();
+		}
+		e.permute(arr.length);
+		return count;
+	}
+
+	// -------------------static methods----------------------
+
+	public static long countTopologicalOrders(Graph g) {
+		EnumerateTopological et = new EnumerateTopological(g);
+		return et.enumerateTopological(false);
+	}
+
+	public static long enumerateTopologicalOrders(Graph g) {
+		EnumerateTopological et = new EnumerateTopological(g);
+		return et.enumerateTopological(true);
+	}
+
+	public static void main(String[] args) {
+		int VERBOSE = 0;
+		if(args.length > 0) { VERBOSE = Integer.parseInt(args[0]); }
+		Graph g = Graph.readDirectedGraph(new java.util.Scanner(System.in));
+		Graph.Timer t = new Graph.Timer();
+		long result;
+		if(VERBOSE > 0) {
+			result = enumerateTopologicalOrders(g);
+		} else {
+			result = countTopologicalOrders(g);
+		}
+		System.out.println("\n" + result + "\n" + t.end());
+	}
 
 }
-
--- src/sxb180066/PERT.java
+++ src/sxb180066/PERT.java
@@ -12,76 +12,179 @@ import rbk.Graph.GraphAlgorithm;
 import rbk.Graph.Factory;
 
 import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class PERT extends GraphAlgorithm<PERT.PERTVertex> {
-    public static class PERTVertex implements Factory {
-	public PERTVertex(Vertex u) {
+	int maxTime;
+	int numOfCriticalNodes;
+	Vertex s; // dummy start node
+	Vertex t; //dummy end node
+
+	/**
+	 * PertVertex class
+	 */
+	public static class PERTVertex implements Factory {
+		int duration;
+		int ec;
+		int lc;
+		int slack;
+		boolean isCritical;
+
+		public PERTVertex(Vertex u) {
+			this.ec = 0;
+			this.isCritical = false;
+			this.slack = 0;
+		}
+		public PERTVertex make(Vertex u) { return new PERTVertex(u); }
 	}
-	public PERTVertex make(Vertex u) { return new PERTVertex(u); }
-    }
 
-    public PERT(Graph g) {
-	super(g, new PERTVertex(null));
-    }
-
-    public void setDuration(Vertex u, int d) {
-    }
-    
-    public boolean pert() {
-	return false;
-    }
-    public int ec(Vertex u) {
-	return 0;
-    }
-
-    public int lc(Vertex u) {
-	return 0;
-    }
-
-    public int slack(Vertex u) {
-	return 0;
-    }
-
-    public int criticalPath() {
-	return 0;
-    }
-
-    public boolean critical(Vertex u) {
-	return false;
-    }
-
-    public int numCritical() {
-	return 0;
-    }
-
-    // setDuration(u, duration[u.getIndex()]);
-    public static PERT pert(Graph g, int[] duration) {
-	return null;
-    }
-    
-    public static void main(String[] args) throws Exception {
-	String graph = "11 12   2 4 1   2 5 1   3 5 1   3 6 1   4 7 1   5 7 1   5 8 1   6 8 1   6 9 1   7 10 1   8 10 1   9 10 1      0 3 2 3 2 1 3 2 4 1 0";
-	Scanner in;
-	// If there is a command line argument, use it as file from which
-	// input is read, otherwise use input from string.
-	in = args.length > 0 ? new Scanner(new File(args[0])) : new Scanner(graph);
-	Graph g = Graph.readDirectedGraph(in);
-	g.printGraph(false);
-	
-	PERT p = new PERT(g);
-	for(Vertex u: g) {
-	    p.setDuration(u, in.nextInt());
+	/**
+	 * PERT Constructor
+	 * @param g
+	 */
+	public PERT(Graph g) {
+		super(g, new PERTVertex(null));
+		this.maxTime = 0;
+		this.numOfCriticalNodes = 0;
+		//initializing start and end nodes
+		this.s = g.getVertex(1);
+		this.t = g.getVertex(g.size());
 	}
-	// Run PERT algorithm.  Returns null if g is not a DAG
-	if(p.pert()) {
-	    System.out.println("Invalid graph: not a DAG");
-	} else {
-	    System.out.println("Number of critical vertices: " + p.numCritical());
-	    System.out.println("u\tEC\tLC\tSlack\tCritical");
-	    for(Vertex u: g) {
-		System.out.println(u + "\t" + p.ec(u) + "\t" + p.lc(u) + "\t" + p.slack(u) + "\t" + p.critical(u));
-	    }
+
+	/**
+	 * Method to set duration
+	 * @param u
+	 * @param d
+	 */
+	public void setDuration(Vertex u, int d) {
+		get(u).duration = d;
 	}
-    }
+
+	/**
+	 * Returns false if graph is not DAG
+	 * @return
+	 */
+	public boolean pert() {
+		//running dfs on the graph
+		DFS d = new DFS(g);
+		List<Vertex> topologicalOrder = d.topologicalOrder1();
+
+		//if topologicalOrder is null then the graph is not DAG
+		if(topologicalOrder == null){
+			return false;
+		}
+
+		//Calculation of EC for all nodes
+		for( Vertex u : topologicalOrder){
+			for(Edge e : g.incident(u)){
+				Vertex v = e.otherEnd(u);
+				if(get(u).ec + get(v).duration > get(v).ec){
+					get(v).ec = get(u).ec + get(v).duration;
+				}
+			}
+		}
+
+		//MaxTime is the EC of end node
+		maxTime = get(t).ec;
+
+		//setting lc of all the nodes to maxTime
+		for(Vertex u : g){
+			get(u).lc = maxTime;
+		}
+
+		//Calculation of LC
+		ListIterator<Vertex> reverseTopological = topologicalOrder.listIterator(topologicalOrder.size()); //using reverse iterator
+		while(reverseTopological.hasPrevious()){
+			Vertex u = reverseTopological.previous();
+			for( Edge e : g.incident(u)){
+				Vertex v = e.otherEnd(u);
+				if(get(v).lc - get(v).duration < get(u).lc){
+					get(u).lc = get(v).lc - get(v).duration;
+				}
+			}
+			//Calculation of slack
+			get(u).slack = get(u).lc - get(u).ec;
+			if(get(u).slack == 0){
+				//if slack is 0 then the node is critical
+				get(u).isCritical = true;
+				numOfCriticalNodes++;
+			}
+		}
+		return true;
+	}
+	public int ec(Vertex u) {
+		return get(u).ec;
+	}
+
+	public int lc(Vertex u) {
+		return get(u).lc;
+	}
+
+	public int slack(Vertex u) {
+		return get(u).slack;
+	}
+
+	public int criticalPath() {
+		return maxTime;
+	}
+
+	public boolean critical(Vertex u) {
+		return get(u).isCritical;
+	}
+
+	public int numCritical() {
+		return numOfCriticalNodes;
+	}
+
+	// setDuration(u, duration[u.getIndex()]);
+	public static PERT pert(Graph g, int[] duration) {
+		PERT pert = new PERT(g);
+
+		//Initializing duration of all nodes and setting up dummy start and end nodes
+		for(Vertex u: g) {
+			pert.setDuration(u, duration[u.getIndex()]);
+			//creating edge from start node to all other nodes
+			if(u.getName() != 1){
+				g.addEdge(pert.s.getIndex(),u.getIndex(),0);
+			}
+			//creating edge from all the nodes to end node
+			if(u.getName() != g.size()){
+				g.addEdge(u.getIndex(),pert.t.getIndex(),0);
+			}
+
+		}
+		if(pert.pert()){
+			return pert;
+		}else{
+			return null;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		String graph = "11 12   2 4 1   2 5 1   3 5 1   3 6 1   4 7 1   5 7 1   5 8 1   6 8 1   6 9 1   7 10 1   8 10 1   9 10 1      0 3 2 3 2 1 3 2 4 1 0";
+		Scanner in;
+		// If there is a command line argument, use it as file from which
+		// input is read, otherwise use input from string.
+		in = args.length > 0 ? new Scanner(new File(args[0])) : new Scanner(graph);
+		Graph g = Graph.readDirectedGraph(in);
+		g.printGraph(false);
+
+		PERT p = new PERT(g);
+		for(Vertex u: g) {
+			p.setDuration(u, in.nextInt());
+		}
+		// Run PERT algorithm.  Returns null if g is not a DAG
+		if(p.pert()) {
+			System.out.println("Invalid graph: not a DAG");
+		} else {
+			System.out.println("Number of critical vertices: " + p.numCritical());
+			System.out.println("u\tEC\tLC\tSlack\tCritical");
+			for(Vertex u: g) {
+				System.out.println(u + "\t" + p.ec(u) + "\t" + p.lc(u) + "\t" + p.slack(u) + "\t" + p.critical(u));
+			}
+		}
+	}
 }
